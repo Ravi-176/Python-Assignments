99a31d105825793c8a087b642c4cd28ecc66a9d8
Somesh Bhattacharya <somesh065@gmail.com>
2024-02-12 20:15:23 +0530

    Initial commit

---
+++ assignment5.py
@@ -0,0 +1,24 @@
+#Palindrome checker
+def isPalindrome():
+    str = input("Enter a string to check:")
+    rev = str[::-1]
+    if str==rev:
+        print("It's a Palindrome")
+    else:
+        print("Sorry,it isn't a Palindrome")
+#Factorial
+def calcFactorial(num):
+    fact=1
+    if num<0:
+        return -1
+    else:
+        for i in range(1,num+1):
+                fact*=i
+    return fact
+ip = int(input("Enter 1 for Palindrome and 2 for factorial\n"))
+if ip==1:
+    isPalindrome()
+else:
+    n = int(input("Enter a number to get your factorial\n"))
+    ans = calcFactorial(n)
+    print("The factorial is:"+str(ans)+"!Thanks and have nice day!")
